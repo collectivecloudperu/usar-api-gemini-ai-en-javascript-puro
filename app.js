17f302beb42b8590f2a96fd1af436ce7dd3f7f2c
Nube Colectiva <collectivecloudperu@users.noreply.github.com>
2025-09-19 22:04:52 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const API_KEY = "";
+
+async function consultarGemini(prompt) {
+    const res = await fetch(
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${API_KEY}`,
+        {
+            method: "POST",
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify({ contents: [{ parts: [ {text: prompt }] }] })
+        }
+    );
+    const data = await res.json();
+    console.log(data.candidates?.[0]?.content?.parts?.[0]?.text || "Sin respuesta")
+}
+
+consultarGemini("Donde está Egipto?");
