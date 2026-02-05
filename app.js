52a7ea4385225092671a373e43c1a38a690263df
collectivecloudperu <collectivecloudperu@gmail.com>
2026-02-04 19:53:03 -0500

    Cambios

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const API_KEY = "";
+const API_KEY = "12345";
 
 async function consultarGemini(prompt) {
-    const res = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${API_KEY}`,
-        {
-            method: "POST",
-            headers: { "Content-Type": "application/json"},
-            body: JSON.stringify({ contents: [{ parts: [ {text: prompt }] }] })
-        }
-    );
-    const data = await res.json();
-    console.log(data.candidates?.[0]?.content?.parts?.[0]?.text || "Sin respuesta")
+  const res = await fetch(
+    `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${API_KEY}`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ contents: [{ parts: [{ text: prompt }] }] }),
+    },
+  );
+  const data = await res.json();
+  console.log(
+    data.candidates?.[0]?.content?.parts?.[0]?.text || "Sin respuesta",
+  );
 }
 
 consultarGemini("Donde está Egipto?");
